dcd9ae22d7964362d962ff41effddb8fca5c2050
Kostadin Bundov <Kostadinbundov@gmail.com>
2024-10-18 13:34:38 +0300

    Refactor code

--- Seminars and Workshops/Week.02/02.Intersections_of_Two_Arrays/02.Intersections_of_Two_Arrays.cpp
+++ Seminars and Workshops/Week.02/02.Intersections_of_Two_Arrays/02.Intersections_of_Two_Arrays.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -8,17 +8,12 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
 
-    int index1 = 0;
-    int index2 = 0;
-
     vector<int> result;
 
-    for (int i = 0; i < nums1.size(); i++)
-    {
+    for (int i = 0; i < nums1.size(); i++) {
         auto it = find(nums2.begin(), nums2.end(), nums1[i]);
 
-        if (it != nums2.end() && count(result.begin(), result.end(), *it))
-        {
+        if (it != nums2.end() && count(result.begin(), result.end(), *it) == 0) {
             result.push_back(*it);
         }
     }
@@ -26,7 +21,18 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
     return result;
 }
 
-int main()
-{
+void printArr(const vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
 
+    cout << endl;
+}
+
+int main() {
+    vector<int> firstArr = { 2, 4, 2, 2 };
+    vector<int> secondArr = { 5, 6, 2, 1, 4 };
+
+    vector<int> intersectionArr = intersection(firstArr, secondArr);
+    printArr(intersectionArr);
 }
--- Seminars and Workshops/Week.02/03.K_Closest_Points_to_Origin/03.K_Closest_Points_to_Origin.cpp
+++ Seminars and Workshops/Week.02/03.K_Closest_Points_to_Origin/03.K_Closest_Points_to_Origin.cpp
@@ -1,50 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-
-void sort(vector<pair<double, vector<int>>>& arr) {
-
-	for (int i = 0; i < arr.size() - 1; i++) {
-		unsigned smallestElementIndex = i;
-
-		for (int j = i + 1; j < arr.size(); j++) {
-			if (arr[smallestElementIndex].first > arr[j].first) {
-				smallestElementIndex = j;
-			}
-		}
-
-		if (smallestElementIndex != i) {
-			std::swap(arr[i], arr[smallestElementIndex]);
-		}
-	}
-}
 
 vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
 	vector<pair<double, vector<int>>> results(points.size());
 
-	for (int i = 0; i < results.size(); i++)
-	{
+	for (int i = 0; i < results.size(); i++) {
 		results[i].first = sqrt(points[i][0] * points[i][0] + points[i][1] * points[i][1]);
 		results[i].second = points[i];
 	}
 
-	sort(results);
+	sort(results.begin(), results.end(), [](const pair<double, vector<int>>& left, const pair< double, vector<int>>& right)
+		{return left.first < right.first; });
 
 	vector<vector<int>> finalResult(k);
 
-	for (int i = 0; i < k; i++)
-	{
+	for (int i = 0; i < k; i++) {
 		finalResult[i] = results[i].second;
 	}
 
 	return finalResult;
 }
 
-int main()
-{
-	vector<vector<int>> points = { {1, 3}, {-2, 2}};
+int main() {
+	vector<vector<int>> points = { {1, 3}, {-2, 2} };
 	int k = 1;
 
 	vector<vector<int>> result = kClosest(points, k);
